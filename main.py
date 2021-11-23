9b5e851246caaa2d23753bffe65c0584acd37990
DPS2004 <dpsophinos@gmail.com>
2021-11-23 10:54:21 -0500

    fix

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import sys
 import os
 
 screenshot_pos = (350,203,1216,682)
-delay = 2
+delay = 1.5
 world = 1
-start_radius = 1
-max_radius = 3
+start_radius = 7
+max_radius = 10
 screenshots = 0
 
 def xytonwse(x,y):
@@ -30,7 +30,7 @@ def screenshot(x,y):
     if p != "":
         p = "_"+p
     webbrowser.open('https://www.doodlemud.com/' + "#w" + str(world)+p)
-    pyautogui.moveTo(1560,205+(screenshots%2)*2,1)
+    pyautogui.moveTo(1560,205+(screenshots%2)*1,1)
     screenshots += 1
     pyautogui.click() 
     time.sleep(delay)
@@ -42,13 +42,16 @@ except:
     print("dir already exists")
 
 
-screenshot(0,0)
+
 for cur_radius in range(start_radius,max_radius+1):
     print(cur_radius)
-    for i in range(0-cur_radius, cur_radius):
-        screenshot(i, cur_radius) 
-        screenshot(cur_radius, 0-i)
-        screenshot(0-i,0-cur_radius)
-        screenshot(0-cur_radius,i)
+    if cur_radius==0:
+        screenshot(0,0)
+    else:
+        for i in range(0-cur_radius, cur_radius):
+            screenshot(i, cur_radius) 
+            screenshot(cur_radius, 0-i)
+            screenshot(0-i,0-cur_radius)
+            screenshot(0-cur_radius,i)
 
 
